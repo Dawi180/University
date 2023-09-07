16290420ed9759990118638d1eecf3490f6aebd0
DAWID JASIECZKO <dawidjasieczko@gmail.com>
2023-09-07 09:39:57 +0200

    Task 2 Start

---
+++ University.Services.Tests/DataAccessService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Xml.Linq;
+using University.Services;
+
+[TestClass]
+public class DataAccessServiceTests
+{
+    [TestMethod]
+    public void SaveData_Success()
+    {
+        // Arrange
+        var data = new MyData { Name = "John", Age = 30 };
+        var filePath = "test.json";
+
+        var fileSystemMock = new Mock<IFileSystem>();
+        fileSystemMock.Setup(fs => fs.WriteAllText(filePath, It.IsAny<string>()));
+
+        var dataService = new DataAccessService<MyData>(fileSystemMock.Object);
+
+        // Act
+        dataService.SaveData(data, filePath);
+
+        // Assert
+        fileSystemMock.Verify(fs => fs.WriteAllText(filePath, It.IsAny<string>()), Times.Once);
+    }
+
+    [TestMethod]
+    public void ReadData_Success()
+    {
+        // Arrange
+        var filePath = "test.json";
+        var jsonData = "{\"Name\":\"John\",\"Age\":30}";
+
+        var fileSystemMock = new Mock<IFileSystem>();
+        fileSystemMock.Setup(fs => fs.Exists(filePath)).Returns(true);
+        fileSystemMock.Setup(fs => fs.ReadAllText(filePath)).Returns(jsonData);
+
+        var dataService = new DataAccessService<MyData>(fileSystemMock.Object);
+
+        // Act
+        var loadedData = dataService.ReadData(filePath);
+
+        // Assert
+        Assert.IsNotNull(loadedData);
+        Assert.AreEqual("John", loadedData.Name);
+        Assert.AreEqual(30, loadedData.Age);
+    }
+}
---
+++ University.Services/DataAccessService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using University.Controls;
+using Newtonsoft.Json;
+using University.Interfaces;
+
+namespace University.Services
+{
+    
+    public class DataAccessService<T> : IDataAccessService<T>
+    {
+        public void SaveData(T data, string filePath)
+        {
+            string jsonData = JsonConvert.SerializeObject(data);
+            File.WriteAllText(filePath, jsonData);
+        }
+
+        public T ReadData(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                string jsonData = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<T>(jsonData);
+            }
+            else
+            {
+                throw new FileNotFoundException("File not found", filePath);
+            }
+        }
+    }
+}
